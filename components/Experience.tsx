98ef46065f16828bbe1823a9524d6ffc7ca4c9a4
pyke-jason <pykeja@uw.edu>
2022-09-28 20:34:03 -0700

    Rewrite various bullet points

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,10 +12,10 @@ export default function Experience({ data, onBecameActive }: PageProps) {
                 date="July 2022 &ndash; Present"
             >
                 <BulletedNotes
-                    notes={["Designed authoritative multiplayer game server architecture for a mixed reality wargaming application using System.NET ",
-                        "Improved framerate on Microsoft HoloLens 2 by 63% through computation offloading and algorithm optimization as well as implementing an object pooling system and memory allocation reduction",
-                        "Reduced network traffic by 37% by implementing allocation-free message encoding/decoding",
-                        "Leveraged knowledge in client/server architecture, UDP/TCP and Transport Layer Security, programmed in C# using Visual Studio, and debugged using Visual Studio, Unity Profiler, and HoloLens Mixed Reality Portal"
+                    notes={["Designed authoritative multiplayer game server architecture for a mixed reality wargaming application using .NET core and Unity with state synchronization and reliable packet sequencing",
+                        "Improved framerate on Microsoft HoloLens 2 by 63% through computation offloading, codebase refactoring and optimization, and object pooling",
+                        "Reduced server network traffic by 37% by implementing allocation-free message serialization/deserialization",
+                        "Leveraged knowledge in client/server architecture, transport protocols (UDP and TCP), programmed in C# using Visual Studio, and debugged using Visual Studio, Unity Profiler, and HoloLens Mixed Reality Portal"
                     ]} />
             </SectionEntry>
             <SectionEntry
@@ -25,7 +25,7 @@ export default function Experience({ data, onBecameActive }: PageProps) {
             >
                 <BulletedNotes
                     notes={[
-                        "Created and validated a patent-pending automated image screening algorithm for arthritis with 95.5% accuracy to a technician assisted method using OpenCV and Human Pose Estimation",
+                        "Created and validated a patent-pending automated image screening algorithm for arthritis with 95.5% accuracy to the technician assisted method using OpenCV and Human Pose Estimation",
                         "Established remote testing and development in HPC system using Docker and bash scripts",
                         "Implemented mobile photo capture and upload pages for REST API using Flutter",
                         "Leveraged knowledge in Mobile Application Development and Computer Vision, programmed using Python and Dart, and debugged using Visual Studio Code"
